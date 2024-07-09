351cf843566a515c897ead59b23c00fee9a3cb77
Minh Thảo Phạm <108984672+Trunks-Pham@users.noreply.github.com>
2024-07-09 16:34:17 +0700

    Add files via upload

--- swagger.js
+++ swagger.js
@@ -1,33 +1,32 @@
-// swagger.js
-const swaggerJSDoc = require('swagger-jsdoc');
+// src/swagger.js
+const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
 
-// Swagger definition
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Express API with Swagger for SOCIAL MEDIA',
-    version: '1.0.0',
-    description: 'Swagger API for SOCIAL MEDIA',
-  },
-  servers: [
-    {
-      url: 'http://localhost:4000',
-      description: 'Local server - API SOCIAL MEDIA',
-    },
-  ],
-};
-
-// Options for the swagger docs
 const options = {
-  swaggerDefinition,
-  apis: [path.join(__dirname, 'src/routers/**/*.js')], // Ensure the path is correct
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Social Media API',
+      version: '1.0.0',
+      description: 'A simple Express Social Media API',
+    },
+    servers: [
+      {
+
+        url: process.env.CLIENT_ENDPOINT,
+        description: 'Production server',
+        // url: 'http://localhost:8080',
+        // description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./src/routers/post/*.js', './src/routers/user/*.js','./src/routers/index.js'],
 };
 
-// Initialize swagger-jsdoc
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJsDoc(options);
 
-module.exports = (app) => {
+const setupSwagger = (app) => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
+
+module.exports = setupSwagger;
